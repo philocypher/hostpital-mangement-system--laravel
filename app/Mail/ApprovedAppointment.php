290f5c65359000a8a2889ceb6f9c10a56609bda2
Ace <66782087+philocypher@users.noreply.github.com>
2023-05-29 17:27:40 +0800

    Update ApprovedAppointment.php

--- app/Mail/ApprovedAppointment.php
+++ app/Mail/ApprovedAppointment.php
@@ -30,7 +30,7 @@ class ApprovedAppointment extends Mailable
     {
         return $this
         ->from('support@wellness.com')
-        ->to('shaheen.aal@protonmail.com')
+        ->to('example@example.com')
         ->subject('approved payment')
         ->markdown('emails.appoint');
     }
